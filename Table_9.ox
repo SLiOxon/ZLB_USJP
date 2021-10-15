892c299bdd06dceeb34807188ca46bfc248fe8ea
SLiOxon <89948317+SLiOxon@users.noreply.github.com>
2021-10-15 13:30:41 +0100

    Add files via upload

---
+++ Table_9.ox
@@ -0,0 +1,83 @@
+/**	Sensitivity: Tests with alternative measures of long term rates and thus shorter sample periods
+	Tests ZLB irrelevance hypothesis in US and Japan by testing CSVAR against CKSVAR
+**/
+
+#include <oxstd.oxh>
+#include <oxdraw.oxh>
+#include <oxprob.oxh>
+#import <maximize>
+#include <cksvar.ox>
+
+main()
+{
+	decl R = 1000;							// # of MC simulations for importance sampling to integrate out latent lags
+	decl fLatLags = TRUE;						// if TRUE (default), model has lags of latent variable Y2* (shadow rate) on the RHS
+
+	decl fUseKSVARinit = TRUE;					// if TRUE, estimate KSVAR first and use as initial values for estimation of CKSVAR
+
+	decl p = 2;
+	decl b = 0.07;					// lower bound = IOR+0.07 for JP
+	decl mresults = constant(.NaN,1,8);	// store p, lik, cp, pv-p, aic, dlr, df, pval
+	
+	///////////////////////////////////////////////////////////////////////
+	//	Setup the model	
+		decl model = new CKSVAR();					// create object, holds all functions needed to estimated the model
+		model.SetBound(b);
+		model.SetNumMCreps(R);
+		model.SetLatentLags(fLatLags);
+		decl sdata = sprint("JPNdata.csv");
+		model.LoadCsv(sdata);	// load data
+		model.Deterministic(FALSE);					// create constant term in database (change to "TRUE" to create seasonal dummies, too)
+		model.Select(CKSVAR::Y1_VAR, {"INFL", 0, p, "YGAP_BOJ", 0, p, "LR10Y", 0, p});	  // Select Y1 (unconstrained) variables in the SVAR
+		decl sSHORTRATE = "SR";
+		model.Select(CKSVAR::Y2_VAR, {sSHORTRATE, 0, p});	   // Selects the variable that is subject to a lower bound constraint, Y2 (here, the call rate).
+		model.Select(CKSVAR::X_VAR, {"Constant", 0, 0, "PGR", 0, 0});	// Select any other deterministic or exogenous regressors
+		model.Select(CKSVAR::Y2_BOUNDS, {"IOR", 0, 0}); // Variable ELB as in Hayashi and Koeda
+			
+		decl iYear0 = 1987;
+		model.SetSelSample(/*Start Year*/iYear0,/*Start Period*/ 4, /*End Year*/ -1, /*End Period*/ -1);		// -1 defaults to earliest (for start) or latest (for end) available dates in the sample 
+
+		ranseed(-1);
+		
+	//  Estimate unsrestricted model and record results		
+		decl vp;
+		if(fUseKSVARinit){
+			decl ksvar = clone(model);
+			ksvar.SetLatentLags(FALSE);
+			ksvar.SetPrint(FALSE);
+			ksvar.Estimate();
+			decl vpK = ksvar.GetPar();
+			decl asNamesK = ksvar.GetParNames();
+			delete ksvar;
+			model.InitPar();
+			vp = model.GetPar();
+			vp[find(model.GetParNames(),asNamesK)] = vpK;			
+			model.SetStartPar(vp);				// initialize at KSVAR estimates
+		}
+	
+		decl time = timer();
+			
+		model.Estimate();								// estimates the model by ML and stores results inside the model object using BFGS
+		decl aic = (-2 * model.GetLogLik() + 2 * model.GetcDfLoss())/model.GetcT();
+		mresults[0][:2] = p~model.GetLogLik()~model.GetParCount();
+		mresults[0][4] = aic;
+		println("\nlapsed time: ", timespan(time),"\n");
+	    model.SetUnrestrictedModel(clone(model));		// store settings of unrestricted CKSVAR model, to test against
+
+	//  Impose CSVAR restrictions and estimate	
+		decl asNamesCK = model.GetParNames();
+		vp = model.GetPar();							// store final estimates
+		model.SetCSVAR(TRUE);							// impose CSVAR restrictions
+		model.InitPar();					
+		model.SetStartPar(vp[find(asNamesCK,model.GetParNames())]);		// use estimates of CKSVAR as initial conditions
+		
+		println("\n*****   Started estimation of the restricted model");	
+		model.Estimate();
+		decl atest = model.TestAgainstUnrestr();
+		if(atest)
+			mresults[0][5:] = atest[0]~atest[1]~atest[2];
+		delete model;
+		println("\nlapsed time: ", timespan(time),"\n");
+		decl asnames = {"p", "lik", "par","pv-p", "aic", "LR", "df", "pval"};
+		println("Results", "%c", asnames, mresults);
+}
---
+++ plotdata.m
@@ -0,0 +1,364 @@
+close all;
+clear all;
+clc;
+
+%% US IRF (Figure 5)
+Dinit = readmatrix("IRFlimits_US.csv"); %file must have no header and different series must be in columns
+Dinit = Dinit(:, 2:end);
+
+D(:,1:5) = Dinit(:,1:5);
+D(:,6:9) = Dinit(:,14:17);
+D(:,10:13) = Dinit(:,6:9);
+D(:,14:17) = Dinit(:,18:21);
+D(:,18:21) = Dinit(:,10:13);
+D(:,22:25) = Dinit(:,22:25);
+
+x = D(:,1)';
+
+% D_lim 6 by 2, first column min, second column max, first 3 rows for 1999,
+% second 3 rows for 2009
+D_lim = [min(D(:, [4 8 12 16 20 24])', [], 2), max(D(:, [5 9 13 17 21 25])', [], 2)];							% store limits for next plot
+D_lim(:,1) = (D_lim(:,1) >= 0).*D_lim(:,1)/1.1 + (D_lim(:,1) < 0).*D_lim(:,1)*1.1;
+D_lim(:,2) = (D_lim(:,2) >= 0).*D_lim(:,2)*1.1 + (D_lim(:,2) < 0).*D_lim(:,2)/1.1;
+D_lim([5 6], 1) = -0.25;
+
+areacol = [0 0 1]/1.4;
+areacol_s = [1 1 1];
+transp = 0.3;
+linwid = 3;
+axiswid = 1;
+
+figure
+
+for i = 1:6
+subplot(3,2,i);
+plot(x,0*x,'k','LineWidth',axiswid);
+hold on
+le = plot(x,D(:,4*i)',':r','LineWidth',axiswid,'DisplayName','67% error bands');
+plot(x,D(:,4*i+1)',':r','LineWidth',axiswid);
+
+xlim([min(x) max(x)]);
+ylim([D_lim(i,1) D_lim(i,2)]);
+
+p=patch([x fliplr(x)], [D(:,4*i-2)' fliplr(D(:,4*i-1)')], areacol);
+p.FaceAlpha = transp;
+p.EdgeAlpha = 0;
+
+if i == 1 
+    title('IRF to monetary policy shock in 1999Q1'); 
+    ylabel('Inflation');
+    legend(le);
+end
+if i == 2 
+    title('IRF to monetary policy shock in 2009Q1'); 
+    legend(le);
+end
+if i == 3 
+    ylabel('Output Gap'); 
+end
+if i == 5 
+    ylabel('Federal Funds Rate'); 
+end
+
+end
+
+%% Japan IRF (Figure 6)
+
+Dinit = readmatrix("IRFlimits_JP.csv"); %file must have no header and different series must be in columns
+Dinit = Dinit(:, 2:end);
+
+D(:,1:5) = Dinit(:,1:5);
+D(:,6:9) = Dinit(:,14:17);
+D(:,10:13) = Dinit(:,6:9);
+D(:,14:17) = Dinit(:,18:21);
+D(:,18:21) = Dinit(:,10:13);
+D(:,22:25) = Dinit(:,22:25);
+
+x = D(:,1)';
+
+% D_lim 6 by 2, first column min, second column max, first 3 rows for 1999,
+% second 3 rows for 2009
+D_lim = [min(D(:, [4 8 12 16 20 24])', [], 2), max(D(:, [5 9 13 17 21 25])', [], 2)];							% store limits for next plot
+D_lim(:,1) = (D_lim(:,1) >= 0).*D_lim(:,1)/1.1 + (D_lim(:,1) < 0).*D_lim(:,1)*1.1;
+D_lim(:,2) = (D_lim(:,2) >= 0).*D_lim(:,2)*1.1 + (D_lim(:,2) < 0).*D_lim(:,2)/1.1;
+D_lim([5 6], 1) = -0.25;
+
+areacol = [0 0 1]/1.4;
+areacol_s = [1 1 1];
+transp = 0.3;
+linwid = 3;
+axiswid = 1;
+
+figure
+
+for i = 1:6
+subplot(3,2,i);
+plot(x,0*x,'k','LineWidth',axiswid);
+hold on
+le = plot(x,D(:,4*i)',':r','LineWidth',axiswid, 'DisplayName','67% error bands');
+plot(x,D(:,4*i+1)',':r','LineWidth',axiswid);
+
+xlim([min(x) max(x)]);
+ylim([D_lim(i,1) D_lim(i,2)]);
+
+p=patch([x fliplr(x)], [D(:,4*i-2)' fliplr(D(:,4*i-1)')], areacol);
+p.FaceAlpha = transp;
+p.EdgeAlpha = 0;
+
+if i == 1 
+    title('IRF to monetary policy shock in 1990Q1'); 
+    ylabel('Inflation');
+    legend(le);
+end
+if i == 2 
+    title('IRF to monetary policy shock in 2010Q1'); 
+    legend(le);
+end
+if i == 3 
+    ylabel('Output Gap'); 
+end
+if i == 5 
+    ylabel('Call Rate'); 
+end
+
+end
+
+%% US Cumulative (Figure 7)
+
+D = readmatrix("IRFlimits_horz_US.csv"); %file must have no header and different series must be in columns
+D = D(:, 2:end);
+D(131:135, [3:4,9:10,15:16]) = NaN;
+D(127:135, [5:6,11:12,17:18]) = NaN;
+
+% D_lim 9 by 2, first column min, second column max, first 3 rows for 1999,
+% second 3 rows for 2009
+D_lim = [min(D(:, [1 3 5 7 9 11 13 15 17])', [], 2), max(D(:, [2 4 6 8 10 12 14 16 18])', [], 2)];							% store limits for next plot
+D_lim(:,1) = (D_lim(:,1) >= 0).*D_lim(:,1)/1.1 + (D_lim(:,1) < 0).*D_lim(:,1)*1.1;
+D_lim(:,2) = (D_lim(:,2) >= 0).*D_lim(:,2)*1.1 + (D_lim(:,2) < 0).*D_lim(:,2)/1.1;
+
+areacol = [0 0 1]/1.4;
+areacol_s = [1 1 1];
+transp = 0.3;
+linwid = 3;
+axiswid = 1;
+
+figure
+starttime = datenum('01-jul-1985','dd-mmm-yyyy');
+endtime = datenum('01-jan-2019','dd-mmm-yyyy');
+xdata = linspace(starttime, endtime, 135);
+endtime2 = datenum('01-oct-2017','dd-mmm-yyyy');
+xdata2 = linspace(starttime, endtime, 130);
+endtime3 = datenum('01-oct-2016','dd-mmm-yyyy');
+xdata3 = linspace(starttime, endtime, 126);
+
+for i = 1:9
+subplot(3,3,i);
+
+if (i==1)|(i==4)|(i==7)
+    plot(xdata,0*xdata,'k','LineWidth',axiswid);
+    hold on
+
+    xlim([min(xdata) max(xdata)]);
+    ylim([D_lim(i,1) D_lim(i,2)]);
+    
+    p=patch([xdata fliplr(xdata)], [D(:,2*i-1)' fliplr(D(:,2*i)')], areacol);
+    p.FaceAlpha = transp;
+    p.EdgeAlpha = 0;
+    
+    datetick('x','yyyy','keeplimits');
+end
+if (i==2)|(i==5)|(i==8)
+    plot(xdata2,0*xdata2,'k','LineWidth',axiswid);
+    hold on
+
+    xlim([min(xdata2) max(xdata2)]);
+    ylim([D_lim(i,1) D_lim(i,2)]);
+    
+    p=patch([xdata2 fliplr(xdata2)], [D(1:130,2*i-1)' fliplr(D(1:130,2*i)')], areacol);
+    p.FaceAlpha = transp;
+    p.EdgeAlpha = 0;
+    
+    datetick('x','yyyy','keeplimits');
+end
+if (i==3)|(i==6)|(i==9)
+    plot(xdata3,0*xdata3,'k','LineWidth',axiswid);
+    hold on
+
+    xlim([min(xdata3) max(xdata3)]);
+    ylim([D_lim(i,1) D_lim(i,2)]);
+    
+    p=patch([xdata3 fliplr(xdata3)], [D(1:126,2*i-1)' fliplr(D(1:126,2*i)')], areacol);
+    p.FaceAlpha = transp;
+    p.EdgeAlpha = 0;
+    
+    datetick('x','yyyy','keeplimits');
+end
+% p.FaceAlpha = transp;
+% p.EdgeAlpha = 0;
+% candystripe(p, 'Color',areacol_s,'Width',1);
+
+if i == 1 
+    title('On impact'); 
+    ylabel('Inflation');
+end
+if i == 2 
+    title('4 quarters ahead'); 
+end
+if i == 3 
+    title('8 quarters ahead'); 
+end
+if i == 4
+    ylabel('Output Gap');
+end
+if i == 7
+    ylabel('Federal Funds Rate'); 
+end
+
+end
+
+%% Japan Cumulative (Figure 8)
+
+D = readmatrix("IRFlimits_horz_JP.csv"); %file must have no header and different series must be in columns
+D = D(:, 2:end);
+D(131:135, [3:4,9:10,15:16]) = NaN;
+D(127:135, [5:6,11:12,17:18]) = NaN;
+
+% D_lim 9 by 2, first column min, second column max, first 3 rows for 1999,
+% second 3 rows for 2009
+D_lim = [min(D(:, [1 3 5 7 9 11 13 15 17])', [], 2), max(D(:, [2 4 6 8 10 12 14 16 18])', [], 2)];							% store limits for next plot
+D_lim(:,1) = (D_lim(:,1) >= 0).*D_lim(:,1)/1.1 + (D_lim(:,1) < 0).*D_lim(:,1)*1.1;
+D_lim(:,2) = (D_lim(:,2) >= 0).*D_lim(:,2)*1.1 + (D_lim(:,2) < 0).*D_lim(:,2)/1.1;
+
+areacol = [0 0 1]/1.4;
+areacol_s = [1 1 1];
+transp = 0.3;
+linwid = 3;
+axiswid = 1;
+
+figure
+starttime = datenum('01-jul-1985','dd-mmm-yyyy');
+endtime = datenum('01-jan-2019','dd-mmm-yyyy');
+xdata = linspace(starttime, endtime, 135);
+endtime2 = datenum('01-oct-2017','dd-mmm-yyyy');
+xdata2 = linspace(starttime, endtime, 130);
+endtime3 = datenum('01-oct-2016','dd-mmm-yyyy');
+xdata3 = linspace(starttime, endtime, 126);
+
+for i = 1:9
+subplot(3,3,i);
+
+if (i==1)|(i==4)|(i==7)
+    plot(xdata,0*xdata,'k','LineWidth',axiswid);
+    hold on
+
+    xlim([min(xdata) max(xdata)]);
+    ylim([D_lim(i,1) D_lim(i,2)]);
+    
+    p=patch([xdata fliplr(xdata)], [D(:,2*i-1)' fliplr(D(:,2*i)')], areacol);
+    p.FaceAlpha = transp;
+    p.EdgeAlpha = 0;
+    
+    datetick('x','yyyy','keeplimits');
+end
+if (i==2)|(i==5)|(i==8)
+    plot(xdata2,0*xdata2,'k','LineWidth',axiswid);
+    hold on
+
+    xlim([min(xdata2) max(xdata2)]);
+    ylim([D_lim(i,1) D_lim(i,2)]);
+    
+    p=patch([xdata2 fliplr(xdata2)], [D(1:130,2*i-1)' fliplr(D(1:130,2*i)')], areacol);
+    p.FaceAlpha = transp;
+    p.EdgeAlpha = 0;
+    
+    datetick('x','yyyy','keeplimits');
+end
+if (i==3)|(i==6)|(i==9)
+    plot(xdata3,0*xdata3,'k','LineWidth',axiswid);
+    hold on
+
+    xlim([min(xdata3) max(xdata3)]);
+    ylim([D_lim(i,1) D_lim(i,2)]);
+    
+    p=patch([xdata3 fliplr(xdata3)], [D(1:126,2*i-1)' fliplr(D(1:126,2*i)')], areacol);
+    p.FaceAlpha = transp;
+    p.EdgeAlpha = 0;
+    
+    datetick('x','yyyy','keeplimits');
+end
+% p.FaceAlpha = transp;
+% p.EdgeAlpha = 0;
+% candystripe(p, 'Color',areacol_s,'Width',1);
+
+if i == 1 
+    title('On impact'); 
+    ylabel('Inflation');
+end
+if i == 2 
+    title('4 quarters ahead'); 
+end
+if i == 3 
+    title('8 quarters ahead'); 
+end
+if i == 4
+    ylabel('Output Gap');
+end
+if i == 7
+    ylabel('Call Rate'); 
+end
+
+end
+
+%% US Shadow Rate (Figure 9)
+
+D = readmatrix("shadowlimits_US.csv"); %file must have no header and different series must be in columns
+D = D(:, 2:end);
+
+areacol = [0 0 1]/1.4;
+areacol_s = [1 1 1];
+transp = 0.3;
+linwid = 3;
+axiswid = 1;
+
+figure
+starttime = datenum('01-jul-1985','dd-mmm-yyyy');
+endtime = datenum('01-jan-2019','dd-mmm-yyyy');
+xdata = linspace(starttime, endtime, 135);
+
+plot(xdata,0*xdata,'k','LineWidth',axiswid);
+hold on
+
+xlim([min(xdata) max(xdata)]);
+    
+p=patch([xdata fliplr(xdata)], [D(:,1)' fliplr(D(:,2)')], areacol);
+p.FaceAlpha = transp;
+p.EdgeAlpha = 1;
+    
+datetick('x','yyyy','keeplimits');
+
+%% Japan Shadow Rate (Figure 10)
+
+D = readmatrix("shadowlimits_JP.csv"); %file must have no header and different series must be in columns
+D = D(:, 2:end);
+
+areacol = [0 0 1]/1.4;
+areacol_s = [1 1 1];
+transp = 0.3;
+linwid = 3;
+axiswid = 1;
+
+figure
+starttime = datenum('01-jul-1985','dd-mmm-yyyy');
+endtime = datenum('01-jan-2019','dd-mmm-yyyy');
+xdata = linspace(starttime, endtime, 135);
+
+plot(xdata,0*xdata,'k','LineWidth',axiswid);
+hold on
+
+xlim([min(xdata) max(xdata)]);
+    
+p=patch([xdata fliplr(xdata)], [D(:,1)' fliplr(D(:,2)')], areacol);
+p.FaceAlpha = transp;
+p.EdgeAlpha = 1;
+    
+datetick('x','yyyy','keeplimits');
